872d55a430ff34691ea8c645ce5c7bcb0bbadfe2
Tural Mukhtarly <tmukhtarly@mail.ru>
2021-04-13 00:27:16 +0300

    first commit

---
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MyPosts = () =>{
+    return(
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div className='posts'>
+                <div className='item'>
+                    post 1
+                </div>
+                <div className='item'>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+export default MyPosts;
