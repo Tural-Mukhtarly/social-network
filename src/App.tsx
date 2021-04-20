0f5ada1d3e5e7aeef73b71bb63ab61d83681f5fa
Tural Mukhtarly <tmukhtarly@mail.ru>
2021-04-20 23:55:07 +0300

    3 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from 'react'
 import './App.css';
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
-import DeleteComponent from './DeleteComponent';
 
 
 function App() {
   return (
     <div className="app-wrapper">
-     <Header/>
-     <Profile/>
-     <Navbar/>
-     <DeleteComponent/>
+      <Header />
+      <Profile />
+      <Navbar />
     </div>
   );
 }
