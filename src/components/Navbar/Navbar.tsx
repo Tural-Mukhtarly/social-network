d4addf9c7eaedd44a63238c737b082188177e82f
Tural Mukhtarly <tmukhtarly@mail.ru>
2021-04-21 01:01:27 +0300

    20 commit

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import './Navbar.css'
+import { NavLink } from 'react-router-dom';
+import s from './Navbar.module.css'
 
 const Navbar = () =>{
     return(
-            <nav className='nav'>
-                <div><a href="/profile">Profile</a></div>
-                <div><a href="/dialogs">Messages</a></div>
-                <div><a href="/news">News</a></div>
-                <div><a href="/music">Music</a></div>
-                <div><a href="/settings">Settings</a></div>
+            <nav className={s.nav}>
+                <div className={s.item}><NavLink to="/profile" activeClassName={s.active}>Profile</NavLink></div>
+                <div className={s.item}><NavLink to="/dialogs" activeClassName={s.active}>Messages</NavLink></div>
+                <div className={s.item}><a href="/news">News</a></div>
+                <div className={s.item}><a href="/music">Music</a></div>
+                <div className={s.item}><a href="/settings">Settings</a></div>
             </nav>
     )
 }
