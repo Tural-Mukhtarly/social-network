9b67203e5bd9fee8457a5baefaa0991117f7f254
Tural Mukhtarly <tmukhtarly@mail.ru>
2021-04-21 00:19:18 +0300

    4 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
@@ -9,8 +10,9 @@ function App() {
   return (
     <div className="app-wrapper">
       <Header />
-      <Profile />
+      {/* <Profile /> */}
       <Navbar />
+      <Dialogs/>
     </div>
   );
 }
