ae205ea575bdb0f863663bc37946b93be8369672
Tural Mukhtarly <tmukhtarly@mail.ru>
2021-04-21 00:46:18 +0300

    19 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header'
@@ -8,14 +9,16 @@ import Profile from './components/Profile/Profile'
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      {/* <Profile /> */}
-      <Navbar />
-      <div className="app-wrapper-content">
-      <Dialogs/>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/profile" component={Profile} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,11 +4,11 @@ import './Navbar.css'
 const Navbar = () =>{
     return(
             <nav className='nav'>
-                <div><a>Profile</a></div>
-                <div><a>Messages</a></div>
-                <div><a>News</a></div>
-                <div><a>Music</a></div>
-                <div><a>Settings</a></div>
+                <div><a href="/profile">Profile</a></div>
+                <div><a href="/dialogs">Messages</a></div>
+                <div><a href="/news">News</a></div>
+                <div><a href="/music">Music</a></div>
+                <div><a href="/settings">Settings</a></div>
             </nav>
     )
 }
