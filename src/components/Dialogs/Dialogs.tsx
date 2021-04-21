bf0438bb0074bba55e110c13104a5b5047112baf
Tural Mukhtarly <tmukhtarly@mail.ru>
2021-04-21 14:52:05 +0300

    21 commit

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,8 +4,31 @@ import s from './Dialogs.module.css'
 
 function Dialogs() {
     return (
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + " " + s.active}>
+                    Tural
+                </div>
+                <div className={s.dialog}>
+                    Olga
+                </div>
+                <div className={s.dialog}>
+                    Rahib
+                </div>
+                <div className={s.dialog}>
+                    Maqa
+                </div>
+                <div className={s.dialog}>
+                    Emil
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Where are you from</div>
+                <div className={s.message}>Where is your cat</div>
+                <div className={s.message}>Your cat is so sweet</div>
+            </div>
         </div>
     )
 }
