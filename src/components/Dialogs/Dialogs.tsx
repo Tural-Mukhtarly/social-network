4fcafcd475fece682f08f8ae7617759733a5a38e
Tural Mukhtarly <tmukhtarly@mail.ru>
2021-04-21 15:43:33 +0300

    24 commit

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,6 +2,21 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import s from './Dialogs.module.css'
 
+const dialogsData = [
+    { id: 1, name: "Tural" },
+    { id: 2, name: "Rahib" },
+    { id: 3, name: "Meherrem" },
+    { id: 4, name: "Emil" },
+    { id: 5, name: "Seymur" },
+]
+
+const messagesData = [
+    { id: 1, message: "Hello" },
+    { id: 2, message: "How are you?" },
+    { id: 3, message: "Where are you from?" },
+    { id: 4, message: "Where is your cat?" },
+    { id: 5, message: "Your cat is so sweet" },
+]
 
 function Dialogs() {
     return (
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import s from './MyPosts.module.css'
 import Posts from './Posts/Posts'
 
+const postData = [
+    { id: 1, post: 'post', likesCount: 11 },
+    { id: 2, post: 'post', likesCount: 12 },
+    { id: 3, post: 'post', likesCount: 13 }
+]
+
 const MyPosts = () => {
     return (
         <div className={s.postsBlock}>
