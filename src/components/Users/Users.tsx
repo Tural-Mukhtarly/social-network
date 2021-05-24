614dc3dc5020dea3c7c65edd26a763101e68aa2e
Tural Mukhtarly <tmukhtarly@mail.ru>
2021-05-24 23:37:54 +0300

    63 commit

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { isDebuggerStatement } from 'typescript'
@@ -41,8 +42,23 @@ function Users(props: UseresType) {
                             </div>
                             <div>
                                 {u.followed
-                                    ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                                    : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                                    ? <button onClick={() => {
+                                        axios.post(`https://social-network.samuraijs.com/api/1.0/users`)
+                                            .then(response => {
+                                                if (response.data.resultCode === 0) {
+                                                    props.unfollow(u.id)
+                                            }
+                                        })
+                                    }}>Unfollow</button>
+                                    
+                                    : <button onClick={() => {
+                                        axios.post(`https://social-network.samuraijs.com/api/1.0/users`)
+                                            .then(response => {
+                                                if (response.data.resultCode === 0) {
+                                                    props.follow(u.id)
+                                                }
+                                            })
+                                    }}>Follow</button>}
                             </div>
                         </span>
                         <span>
